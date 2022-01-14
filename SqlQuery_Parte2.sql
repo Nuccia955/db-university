9bed21b58f56a261d850c40649ff021646a1bc68
Nuccia955 <elenamunaro1995@gmail.com>
2022-01-14 14:47:34 +0100

    add join queries

--- SqlQuery_Parte2.sql
+++ SqlQuery_Parte2.sql
@@ -23,3 +23,40 @@ GROUP BY `department_id`;
 
 
 
+
+-- JOIN --
+
+-- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+SELECT `students`.* , `degrees`.`name` AS `degree_name`
+FROM `students` 
+INNER JOIN `degrees` ON `students`.`degree_id` = `degrees`.`id`
+WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
+
+-- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+SELECT `degrees`.* , `departments`.`name` AS `department_name`
+FROM `degrees` 
+INNER JOIN `departments` ON `degrees`.`department_id` = `departments`.`id`
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
+
+-- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT `courses`.* , `teachers`.`name` AS `teacher_name` , `teachers`.`surname` AS `teacher_surname`
+FROM `courses`
+INNER JOIN `course_teacher` ON `course_teacher`.`course_id` = `courses`.`id`
+INNER JOIN `teachers` ON `course_teacher`.`teacher_id` = `teachers`.`id`
+WHERE `teachers`.`id` = 44;
+
+-- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+SELECT `students`.* , 
+`degrees`.`name` AS `degree_name`, 
+`degrees`.`level` AS `degree_level`,
+`degrees`.`address` AS `degree_address`,
+`degrees`.`email` AS `degree_email`,
+`degrees`.`website` AS `degree_website`,
+`departments`.`name` AS `department_name`
+FROM `students` 
+INNER JOIN `degrees` ON `students`.`degree_id` = `degrees`.`id`
+INNER JOIN `departments` ON `degrees`.`department_id` = `departments`.`id`
+ORDER BY `students`.`surname`, `students`.`name`;
+
+
+
